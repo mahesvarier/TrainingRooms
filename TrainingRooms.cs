5d11d7f59247aca0e5b9f9fa6ef6b6733dc2aa1c
Mahes Varier <mahesvarier@gmail.com>
2020-05-12 17:32:41 +1000

    Add project files.

---
+++ TrainingRooms.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrainingRooms
+{
+    public class TrainingRooms
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int NumberComputers { get; set; }
+    }
+}
